47bd518e5cda608bba63f9fd0a68143824c4fb99
ilyaabramovich <ilyaabramovich1998@gmail.com>
2020-03-04 17:04:36 +0300

    made new todo input required

--- src/components/AddTodoForm/AddTodoForm.js
+++ src/components/AddTodoForm/AddTodoForm.js
@@ -18,6 +18,7 @@ export const AddTodoForm = ({ onSubmit }) => {
     <form className="add-todo-form" data-testid="form" onSubmit={handleSubmit}>
       <div className="add-todo-form__control">
         <input
+          required
           autoFocus
           aria-label="New thing to do"
           className="input add-todo-form__input add-todo-form__input_fullwidth"
@@ -29,7 +30,6 @@ export const AddTodoForm = ({ onSubmit }) => {
       </div>
       <button
         type="submit"
-        disabled={!text}
         className="button add-todo-form__button add-todo-form__button_fullwidth"
       >
         Add
